2814635f8a6654532dee4c3e34663d7bd03a72aa
Ryan DeWolfe <ryandewolfe33@gmail.com>
2021-03-24 11:01:33 -0700

    Add toSting and flightTime to Flight

--- src/data/Flight.java
+++ src/data/Flight.java
@@ -9,6 +9,7 @@ public class Flight implements Compareable<Flight>{
     private String destinationAirportId;
     private Date arrivalDateTime;
     private double ticketPrice;
+    private int flightTime;
 
     public Flight(String[] dataRow){
         this.originAirportId = dataRow[11];
@@ -18,6 +19,7 @@ public class Flight implements Compareable<Flight>{
         this.arrivalDateTime = new Date(dataRow[0], dataRow[2], dataRow[3], dataRow[41].substring(0,2), dataRow[41].substring(2,4));
 
         this.ticketPrice = makeTicketPrice(Integer.parseInt(dataRow[54]));
+        this.flightTIme = makeFlightTime();
         this.flightName = dataRow[8] + dataRow[10] + " on " + this.departureDateTime.
     }
 
@@ -28,7 +30,21 @@ public class Flight implements Compareable<Flight>{
             normalDistribution = 1;
         return 50+0.11*distanceInMiles*normalDistribution;
     }
+    public double getTicketPrice(){ return this.ticketPrice;}
 
+    //flightTime has the last 2 digits as the minutes, and the first 0-2 digits as the hours
+    private int makeFlightTime(){
+        int flightTime = 0;
+        flightTime = this.arrivalDateTime.getHours()*100 + this.arrivalDateTime.getMinutes();
+        flightTime -= this.departureDateTime.getHours()*100 + this.departureDateTime.getMinutes();
+        if(this.departureDateTime.getDate() < this.arrivalDateTime.getDate())
+            flightTime += 2400;
+        return flightTime;
+    }
+    public getFlightTime(){return this.flightTime;}
+
+
+    //Flights will be ordered lowest to highest originAirportId, then by departureTime
     public int compareTo(Flight other){
         if(this.originAirportId == other.originAirportId)
             if(this.departureDateTime.befor(other.departureDateTime))
@@ -39,7 +55,14 @@ public class Flight implements Compareable<Flight>{
             return (this.originAirportId < other.originAirportId)? -1:1;
     }
 
-
+    public String toString(){
+        return flightName + ", leaving ", + originAirportId + " at " + this.departureDateTime.getMonth() + "/" +
+                this.departureDateTime.getDate() + " " + this.departureDateTime.getHours() + ":" +
+                this.departureDateTime.getMinutes() + " and arriving at " +
+                this.destinationAirportId + " at " + this.arrivalDateTime.getMonth() + "/" +
+                this.arrivalDateTime.getDate() + " " + this.arrivalDateTime.getHours() + ":"+
+                this.arrivalDateTime.getMinutes();
+    }
 
 
 }
