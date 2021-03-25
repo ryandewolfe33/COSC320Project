2109a43dc5348accf8f73cbfbc9bfc2bd4f112f3
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-25 14:41:21 -0700

    Updates Flight

--- src/data/Flight.java
+++ src/data/Flight.java
@@ -1,36 +1,38 @@
-import Random;
 
 package data;
 
-public class Flight implements Compareable<Flight>{
+import java.util.Date;
+import java.util.Random;
+
+public class Flight implements Comparable<Flight>{
     private String flightName;
-    private String originAirportId;
+    private int originAirportId;
     private Date departureDateTime;
-    private String destinationAirportId;
+    private int destinationAirportId;
     private Date arrivalDateTime;
     private double ticketPrice;
     private int flightTime;
 
 
     public Flight(String[] dataRow){
-        this.originAirportId = dataRow[11];
+        this.originAirportId = Integer.parseInt(dataRow[11]);
         this.departureDateTime = new Date(dataRow[0], dataRow[2], dataRow[3], dataRow[30].substring(0,2), dataRow[30].substring(2,4));
 
-        this.destinationAirportId = dataRow[20];
+        this.destinationAirportId = Integer.parseInt(dataRow[20]);
         this.arrivalDateTime = new Date(dataRow[0], dataRow[2], dataRow[3], dataRow[41].substring(0,2), dataRow[41].substring(2,4));
 
         this.ticketPrice = makeTicketPrice(Integer.parseInt(dataRow[54]));
-        this.flightTIme = makeFlightTime();
+        this.flightTime = makeFlightTime();
         this.flightName = dataRow[8] + dataRow[10] + " on " + this.departureDateTime.
     }
 
-    public String getOriginAirportId() {
+    public int getOriginAirportId() {
         return originAirportId;
     }
     public Date getDepartureDateTime() {
         return departureDateTime;
     }
-    public String getDestinationAirportId() {
+    public int getDestinationAirportId() {
         return destinationAirportId;
     }
     public Date getArrivalDateTime() {
@@ -39,7 +41,7 @@ public class Flight implements Compareable<Flight>{
     
     private double makeTicketPrice(int distanceInMiles){
         Random random = new Random();
-        double normalDistribution = (0.02*randomndom.getGaussian())+1  //A variable normally distributed about 1 with std 0.02
+        double normalDistribution = (0.02*random.nextGaussian())+1;  //A variable normally distributed about 1 with std 0.02
         if(normalDistribution<0)
             normalDistribution = 1;
         return 50+0.11*distanceInMiles*normalDistribution;
@@ -55,28 +57,21 @@ public class Flight implements Compareable<Flight>{
             flightTime += 2400;
         return flightTime;
     }
-    public getFlightTime(){return this.flightTime;}
+    public int getFlightTime(){return this.flightTime;}
 
 
     //Flights will be ordered lowest to highest originAirportId, then by departureTime
     public int compareTo(Flight other){
-        if(this.originAirportId == other.originAirportId)
-            if(this.departureDateTime.befor(other.departureDateTime))
-                return -1;
-            else
-                return 1;
-        else
-            return (this.originAirportId < other.originAirportId)? -1:1;
+        return departureDateTime.compareTo(other.departureDateTime);
     }
 
-    public String toString(){
+    /*public String toString(){
         return flightName + ", leaving ", + originAirportId + " at " + this.departureDateTime.getMonth() + "/" +
                 this.departureDateTime.getDate() + " " + this.departureDateTime.getHours() + ":" +
                 this.departureDateTime.getMinutes() + " and arriving at " +
                 this.destinationAirportId + " at " + this.arrivalDateTime.getMonth() + "/" +
                 this.arrivalDateTime.getDate() + " " + this.arrivalDateTime.getHours() + ":"+
                 this.arrivalDateTime.getMinutes();
-    }
-
+    }*/
 
 }
