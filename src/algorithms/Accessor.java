e3b7c6ba2faf7680ba2ae940af5cd2a3ef6cadb2
Michwds <michael.woods@live.ca>
2021-03-25 16:37:19 -0700

    Refactored and updated interface

--- src/algorithms/Accessor.java
+++ src/algorithms/Accessor.java
@@ -1,10 +1,19 @@
 package algorithms;
 
 import data.Flight;
+
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 interface Accessor {
-    public void buildMap();
+    public void buildMapFromArray(Flight[] fly);
 
     public ArrayList<Flight> getNextFlights(Flight arrival);
+
+    public void setStartDate(Calendar newStart);
+
+    public Calendar getStartDate();
+
+    public HashMap<Integer, ArrayList<Flight>> getFlightList();
 }
--- src/algorithms/FlightList.java
+++ src/algorithms/FlightList.java
@@ -1,43 +1,41 @@
 package algorithms;
 
 import data.Flight;
+
 import java.util.HashMap;
-import java.util.Date;
+import java.util.Calendar;
 import java.util.ArrayList;
 
 public class FlightList implements Accessor {
 
-    private HashMap<String, ArrayList<Flight>> flights;
-    private Date startDate;
+    private HashMap<Integer, ArrayList<Flight>> flights;
+    private Calendar startDate;
 
-    public FlightList(Date start) {
+    public FlightList(Calendar start, Flight[] fly) {
         this.startDate = start;
+        buildMapFromArray(fly);
     }
 
-    public void setStartDate(Date newStart) {
+    public void setStartDate(Calendar newStart) {
         this.startDate = newStart;
     }
 
-    public Date getStartDate() {
+    public Calendar getStartDate() {
         return this.startDate;
     }
 
-    public HashMap<String, ArrayList<Flight>> getFlightList() {
+    public HashMap<Integer, ArrayList<Flight>> getFlightList() {
         return this.flights;
     }
 
-    private void buildFromDate(Date start) {
-        this.flights = new HashMap<String, ArrayList<Flight>>(); //String = departure airport ID
+    public void buildMapFromArray(Flight[] fly) {
+        this.flights = new HashMap<Integer, ArrayList<Flight>>(); //String = departure airport ID
         //TODO: call Liza's method to read in csv
     }
 
-    public void buildMap() {
-        buildFromDate(startDate);
-    }
-
     public ArrayList<Flight> getNextFlights(Flight arrival) {
-        String arrID = arrival.getDestinationAirportId();
-        Date arrTime = arrival.getArrivalDateTime();
+        int arrID = arrival.getDestinationAirportId();
+        Calendar arrTime = arrival.getArrivalDateTime();
         ArrayList<Flight> relevant = flights.get(arrID);
         ArrayList<Flight> connections = new ArrayList<Flight>();
         for (int i = 0; i < relevant.size(); i++) {
