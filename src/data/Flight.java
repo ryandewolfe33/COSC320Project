5bb64f5b174e7b4c39ecf199a4e2ab446c03cedb
Ryan DeWolfe <ryandewolfe33@gmail.com>
2021-03-25 15:03:13 -0700

    Fix so many bugs in Flight

--- src/data/Flight.java
+++ src/data/Flight.java
@@ -1,67 +1,77 @@
-import Random;
-
 package data;
+import java.util.Calendar;
+import java.util.Random;
 
-public class Flight implements Compareable<Flight>{
+public class Flight implements Comparable<Flight>{
     private String flightName;
-    private String originAirportId;
-    private Date departureDateTime;
-    private String destinationAirportId;
-    private Date arrivalDateTime;
+    private int originAirportId;
+    private Calendar departureDateTime;
+    private int destinationAirportId;
+    private Calendar arrivalDateTime;
     private double ticketPrice;
     private int flightTime;
 
 
     public Flight(String[] dataRow){
-        this.originAirportId = dataRow[11];
-        this.departureDateTime = new Date(dataRow[0], dataRow[2], dataRow[3], dataRow[30].substring(0,2), dataRow[30].substring(2,4));
+        this.originAirportId = Integer.parseInt(dataRow[11]);
+        this.departureDateTime = Calendar.getInstance();
+        this.departureDateTime.set(Integer.parseInt(dataRow[0]),
+                                   Integer.parseInt(dataRow[2]),
+                                   Integer.parseInt(dataRow[3]),
+                                   Integer.parseInt(dataRow[30].substring(0, 2)),
+                                   Integer.parseInt(dataRow[30].substring(2, 4)));
 
-        this.destinationAirportId = dataRow[20];
-        this.arrivalDateTime = new Date(dataRow[0], dataRow[2], dataRow[3], dataRow[41].substring(0,2), dataRow[41].substring(2,4));
+        this.destinationAirportId = Integer.parseInt(dataRow[20]);
+        this.arrivalDateTime = Calendar.getInstance();
+        this.arrivalDateTime.set(Integer.parseInt(dataRow[0]),
+                Integer.parseInt(dataRow[2]),
+                Integer.parseInt(dataRow[3]),
+                Integer.parseInt(dataRow[41].substring(0, 2)),
+                Integer.parseInt(dataRow[41].substring(2, 4)));
 
         this.ticketPrice = makeTicketPrice(Integer.parseInt(dataRow[54]));
-        this.flightTIme = makeFlightTime();
-        this.flightName = dataRow[8] + dataRow[10] + " on " + this.departureDateTime.
+        this.flightTime = makeFlightTime();
+        this.flightName = dataRow[8] + dataRow[10] + " on " + this.departureDateTime.get(Calendar.DATE);
     }
 
-    public String getOriginAirportId() {
+    public int getOriginAirportId() {
         return originAirportId;
     }
-    public Date getDepartureDateTime() {
+    public Calendar getDepartureDateTime() {
         return departureDateTime;
     }
-    public String getDestinationAirportId() {
+    public int getDestinationAirportId() {
         return destinationAirportId;
     }
-    public Date getArrivalDateTime() {
+    public Calendar getArrivalDateTime() {
         return arrivalDateTime;
     }
     
     private double makeTicketPrice(int distanceInMiles){
         Random random = new Random();
-        double normalDistribution = (0.02*randomndom.getGaussian())+1  //A variable normally distributed about 1 with std 0.02
+
+        //A variable normally distributed about 1 with std 0.02
+        double normalDistribution = (0.02*random.nextGaussian())+1;
+
         if(normalDistribution<0)
             normalDistribution = 1;
         return 50+0.11*distanceInMiles*normalDistribution;
     }
     public double getTicketPrice(){ return this.ticketPrice;}
 
-    //flightTime has the last 2 digits as the minutes, and the first 0-2 digits as the hours
+    //flightTime in minutes
     private int makeFlightTime(){
         int flightTime = 0;
-        flightTime = this.arrivalDateTime.getHours()*100 + this.arrivalDateTime.getMinutes();
-        flightTime -= this.departureDateTime.getHours()*100 + this.departureDateTime.getMinutes();
-        if(this.departureDateTime.getDate() < this.arrivalDateTime.getDate())
-            flightTime += 2400;
+        flightTime = (int)((this.arrivalDateTime.getTimeInMillis() - this.departureDateTime.getTimeInMillis())%60000);
         return flightTime;
     }
-    public getFlightTime(){return this.flightTime;}
+    public int getFlightTime(){return this.flightTime;}
 
 
     //Flights will be ordered lowest to highest originAirportId, then by departureTime
     public int compareTo(Flight other){
         if(this.originAirportId == other.originAirportId)
-            if(this.departureDateTime.befor(other.departureDateTime))
+            if(this.departureDateTime.before(other.departureDateTime))
                 return -1;
             else
                 return 1;
@@ -70,12 +80,10 @@ public class Flight implements Compareable<Flight>{
     }
 
     public String toString(){
-        return flightName + ", leaving ", + originAirportId + " at " + this.departureDateTime.getMonth() + "/" +
-                this.departureDateTime.getDate() + " " + this.departureDateTime.getHours() + ":" +
-                this.departureDateTime.getMinutes() + " and arriving at " +
-                this.destinationAirportId + " at " + this.arrivalDateTime.getMonth() + "/" +
-                this.arrivalDateTime.getDate() + " " + this.arrivalDateTime.getHours() + ":"+
-                this.arrivalDateTime.getMinutes();
+        return flightName + ", leaving " + originAirportId + " at " +  " " +
+                this.departureDateTime.get(Calendar.HOUR_OF_DAY) + ":" + this.departureDateTime.get(Calendar.MINUTE) +
+                " and arriving at " + this.destinationAirportId + " at " + " " +
+                this.arrivalDateTime.get(Calendar.HOUR_OF_DAY) + ":" + this.arrivalDateTime.get(Calendar.MINUTE);
     }
 
 
