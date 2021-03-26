3da1e210b3e75532f1bd56a4781f2f786e796dce
Josh Cooper <cooper.s.josh@outlook.com>
2021-03-25 21:41:17 -0700

    Refactors Flight + revises FlightList
    Flight
    - Refactors to using LocalDateTime instead of Calendar
      - allows us to simply add times onto the date+time (thereby allowing us to calculate the correct date, as well as the time)
    - Revises the compareto method
      - should allow retrieving outgoing flights with the arrival flight, without changing its state
    
    FlightList
    - Refactors to accommodate the change to LocalDateTime
    - Revises to not provide access to the class's inner state

--- src/algorithms/FlightList.java
+++ src/algorithms/FlightList.java
@@ -1,6 +1,8 @@
 package algorithms;
 
 import data.Flight;
+
+import java.time.LocalDateTime;
 import java.util.TreeSet;
 import java.util.Calendar;
 import java.util.HashMap;
@@ -9,42 +11,11 @@ import java.util.ArrayList;
 public class FlightList implements Accessor {
 
     private HashMap<Integer, TreeSet<Flight>> flights;
-    private Calendar startDate;
 
-    public FlightList(Calendar start, Flight[] fly) {
-        this.startDate = start;
-        buildMapFromArray(fly);
-    }
-
-    public FlightList(Flight[] fly) {
-        startDate = null;
-        buildMapFromArray(fly);
-    }
-
-    public FlightList(Calendar start) {
-        this.startDate = start;
-    }
-
-    public FlightList() {
-        startDate = null;
-    }
-
-    public void setStartDate(Calendar newStart) {
-        this.startDate = newStart;
-    }
-
-    public Calendar getStartDate() {
-        return this.startDate;
-    }
-
-    public HashMap<Integer, TreeSet<Flight>> getFlightList() {
-        return this.flights;
-    }
-
-    public void buildMapFromArray(Flight[] fly) {
+    public void buildMapFromArray(Flight[] fly, LocalDateTime startDate) {
         this.flights = new HashMap<>(); //String = departure airport ID
         for (Flight flight : fly) {
-            if (flight.getDepartureDateTime().after(startDate) || startDate == null) {
+            if (startDate == null || flight.getDepartureDateTime().isAfter(startDate)) {
                 TreeSet<Flight> airport_flights = flights.computeIfAbsent(flight.getOriginAirportId(), k -> new TreeSet<>());
                 airport_flights.add(flight);
             }
@@ -53,12 +24,13 @@ public class FlightList implements Accessor {
 
     public ArrayList<Flight> getNextFlights(Flight arrival) {
         int arrID = arrival.getDestinationAirportId();
-        Calendar arrTime = arrival.getArrivalDateTime();
-        arrival.setDepartureDateTime(arrTime);
-        arrival.setOriginAirportId(arrID);
-        TreeSet<Flight> relevant = flights.get(arrID);
-        TreeSet<Flight> tail_set = (TreeSet<Flight>) relevant.tailSet(arrival);
-        return new ArrayList<>(tail_set);
+        /* todo: ensure Flight.compareTo is going to work correctly with the arrivalTime vs departureTime
+        Flight copy = new Flight(arrival);
+        copy.setDepartureDateTime(copy.getArrivalDateTime());
+         */
+        TreeSet<Flight> airport = flights.get(arrID);
+        //return new ArrayList<>(airport.tailSet(copy));
+        return new ArrayList<>(airport.tailSet(arrival));
     }
 
 }
--- src/data/Flight.java
+++ src/data/Flight.java
@@ -1,62 +1,76 @@
 package data;
 
-import java.util.Calendar;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class Flight implements Comparable<Flight>{
+    private static final DateTimeFormatter departure_format = DateTimeFormatter.ofPattern("yyyy-MM-ddHHmm");
+    private static final DateTimeFormatter time_format = DateTimeFormatter.ofPattern("HHmm");
+
     private final String flightName;
-    private final int originAirportId;
-    private final Calendar departureDateTime;
-    private final int destinationAirportId;
-    private final Calendar arrivalDateTime;
     private final double ticketPrice;
     private final int flightTime;
+    private final int distance;
+
+    private final int originAirportId;
+    private final int destinationAirportId;
+
+    private LocalDateTime departureDateTime;
+    private final LocalDateTime arrivalDateTime;
 
 
     public Flight(String[] dataRow){
-        this.originAirportId = Integer.parseInt(dataRow[11]);
-        this.departureDateTime = Calendar.getInstance();
-        this.departureDateTime.set(Integer.parseInt(dataRow[0]),
-                                   Integer.parseInt(dataRow[2]),
-                                   Integer.parseInt(dataRow[3]),
-                                   Integer.parseInt(dataRow[30].substring(0, 2)),
-                                   Integer.parseInt(dataRow[30].substring(2, 4)));
+        originAirportId = Integer.parseInt(dataRow[11]);
+        destinationAirportId = Integer.parseInt(dataRow[20]);
 
-        this.destinationAirportId = Integer.parseInt(dataRow[20]);
-        this.arrivalDateTime = Calendar.getInstance();
-        this.arrivalDateTime.set(Integer.parseInt(dataRow[0]),
-                Integer.parseInt(dataRow[2]),
-                Integer.parseInt(dataRow[3]),
-                Integer.parseInt(dataRow[41].substring(0, 2)),
-                Integer.parseInt(dataRow[41].substring(2, 4)));
+        departureDateTime = LocalDateTime.parse(dataRow[5]+dataRow[30] , departure_format);
+        flightTime = Integer.parseInt(dataRow[41]);
+        int airtime_hours = flightTime / 60;
+        int airtime_minutes = flightTime - (airtime_hours * 60);
+        LocalDateTime arrival_time = departureDateTime.plusHours(airtime_hours).plusMinutes(airtime_minutes);
+        LocalDateTime local_arrival_time = LocalDateTime.parse(dataRow[41],time_format);
+        int timezone_offset_hours = local_arrival_time.getHour() - arrival_time.getHour();
+        int timezone_offset_minutes = local_arrival_time.getMinute() - arrival_time.getMinute();
+        arrivalDateTime = arrival_time
+                .plusHours(timezone_offset_hours).plusMinutes(timezone_offset_minutes)
+                .withHour(local_arrival_time.getHour()).withMinute(local_arrival_time.getMinute());
 
-        this.ticketPrice = makeTicketPrice(Integer.parseInt(dataRow[54]));
-        this.flightTime = makeFlightTime();
-        this.flightName = dataRow[8] + dataRow[10] + " on " + this.departureDateTime.get(Calendar.DATE);
+        distance = Integer.parseInt(dataRow[54]);
+        ticketPrice = makeTicketPrice(distance);
+        flightName = dataRow[8] + dataRow[10] + " on " + departureDateTime.toString();
+    }
+
+    public Flight(Flight other) {
+        this.flightName = other.flightName;
+        this.ticketPrice = other.ticketPrice;
+        this.flightTime = other.flightTime;
+        this.distance = other.distance;
+        this.originAirportId = other.originAirportId;
+        this.destinationAirportId = other.destinationAirportId;
+        this.departureDateTime = other.departureDateTime;
+        this.arrivalDateTime = other.arrivalDateTime;
     }
 
     public int getOriginAirportId(){
         return originAirportId;
     }
-    public void setOriginAirportId(int in){
-        this.originAirportId=in;
-    }
 
-    public Calendar getDepartureDateTime(){
+    public LocalDateTime getDepartureDateTime(){
         return departureDateTime;
     }
-    public void setDepartureDateTime(Calendar in){
+    public void setDepartureDateTime(LocalDateTime in){
         this.departureDateTime=in;
     }
 
     public int getDestinationAirportId(){
         return destinationAirportId;
     }
-    
-    public Calendar getArrivalDateTime(){
+
+    public LocalDateTime getArrivalDateTime(){
         return arrivalDateTime;
     }
-    
+
     private double makeTicketPrice(int distanceInMiles){
         Random random = new Random();
 
@@ -69,29 +83,21 @@ public class Flight implements Comparable<Flight>{
     }
     public double getTicketPrice(){ return this.ticketPrice;}
 
-    private int makeFlightTime(){
-        int flightTime = 0;
-        return flightTime;
-    }
-
-
     //Flights will be ordered lowest to highest originAirportId, then by departureTime
-    public int compareTo(Flight other){
-        if(this.originAirportId == other.originAirportId)
-            if(this.departureDateTime.before(other.departureDateTime))
-                return -1;
-            else
-                return 1;
-        else
-            return (this.originAirportId < other.originAirportId)? -1:1;
+    public int compareTo(Flight other) {
+        if (this.originAirportId == other.originAirportId) {
+            return departureDateTime.compareTo(other.departureDateTime);
+        }
+        return arrivalDateTime.compareTo(other.departureDateTime);/**/
+        //return departureDateTime.compareTo(other.departureDateTime);
     }
 
     public String toString(){
         return flightName + ", leaving " + originAirportId + " at " +  " " +
-                this.departureDateTime.get(Calendar.HOUR_OF_DAY) + ":" + this.departureDateTime.get(Calendar.MINUTE) +
+                this.departureDateTime.getHour() + ":" + this.departureDateTime.getMinute() +
                 " and arriving at " + this.destinationAirportId + " at " + " " +
-                this.arrivalDateTime.get(Calendar.HOUR_OF_DAY) + ":" + this.arrivalDateTime.get(Calendar.MINUTE);
+                this.arrivalDateTime.getHour() + ":" + this.arrivalDateTime.getMinute();
     }
-        }
+}
 
 
