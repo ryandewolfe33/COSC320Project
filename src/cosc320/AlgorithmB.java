40fd601e45c6d70897544468d9b56a9f0b5c594d
Michwds <michael.woods@live.ca>
2021-04-09 14:21:42 -0700

    merged changes

--- src/cosc320/AlgorithmB.java
+++ src/cosc320/AlgorithmB.java
@@ -56,38 +56,25 @@ public class AlgorithmB {
             throw new Exception("Cannot find a path. There appears to be either an issue with the algorithm, or the data.");
         } else {
             Node pathNode = null;
-            for (Flight f : (data.getNextFlights(current_node.getThisFlight()))) {//TODO: fix broken return logic
-                if (!visited.contains(f.getDestinationAirportId()) &&
-                        new Path(recursive(new Node(f.getDestinationAirportId(),
-                        current_node,
-                        f,
-                        data.getNextFlights(f),
-                        f.getFlightTime(),
-                        f.getTicketPrice()), visited, data, B)).compareTo(new Path(pathNode))==-1)
+            Flight this_flight = current_node.getThisFlight();
+            for (Flight next_flight : (data.getNextFlights(current_node.getThisFlight()))) {
+                long time_cost = 0;
+                long layover = 0;
+                if (this_flight != null) {
+                    var edge_A_side = this_flight.getArrivalDateTime();
+                    var edge_B_side = next_flight.getDepartureDateTime();
+                    layover = ChronoUnit.MINUTES.between(edge_A_side, edge_B_side) - this_flight.getTimezoneOffset();
+                } else if (next_flight.getDestinationAirportId() == B) {
+                    continue;
+                }
+                time_cost = layover + next_flight.getFlightTime();
+                if (!visited.contains(next_flight.getDestinationAirportId()) &&
+                        new Path(recursive(new Node(
+                                next_flight.getDestinationAirportId(), current_node, next_flight, data.getNextFlights(next_flight), time_cost, next_flight.getTicketPrice()),
+                                visited, data, B)).compareTo(new Path(pathNode))==-1)
                     pathNode=current_node;
             }
             return pathNode;
-        }
-            return null;
-        } else {
-            Flight this_flight = current_node.getThisFlight();
-            for (Flight next_flight : (data.getNextFlights(this_flight))) {//TODO: fix broken return logic
-                if (!visited.contains(next_flight.getDestinationAirportId())) {
-                    long time_cost = 0;
-                    long layover = 0;
-                    if (this_flight != null) {
-                        var edge_A_side = this_flight.getArrivalDateTime();
-                        var edge_B_side = next_flight.getDepartureDateTime();
-                        layover = ChronoUnit.MINUTES.between(edge_A_side, edge_B_side) - this_flight.getTimezoneOffset();
-                    } else if (next_flight.getDestinationAirportId() == B) {
-                        continue;
-                    }
-                    time_cost = layover + next_flight.getFlightTime();
-                    Node new_node = new Node(next_flight.getDestinationAirportId(), current_node, next_flight,
-                            data.getNextFlights(next_flight), time_cost, next_flight.getTicketPrice());
-                    return recursive(new_node, visited, data, B);
-                }
-            }
         }
     }
 }
