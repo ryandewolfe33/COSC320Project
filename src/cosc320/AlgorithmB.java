f9f1ff78cbf3f6ed0e230c508c646877d532359a
Michwds <michael.woods@live.ca>
2021-04-09 15:19:31 -0700

    updated recursive loop

--- src/cosc320/AlgorithmB.java
+++ src/cosc320/AlgorithmB.java
@@ -9,8 +9,10 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 import java.util.HashSet;
 import java.util.Stack;
+import java.util.TreeSet;
 
 public class AlgorithmB {
+    private TreeSet<Path> paths = new TreeSet<>();
     public AlgorithmB() {
     }
 
@@ -47,35 +49,36 @@ public class AlgorithmB {
         return bestFoundNode;
     }
 
-    private Node recursive(Node current_node, HashSet<Integer> visited, FlightList data, int B) throws Exception {
-        visited.add(current_node.airport_id);
-        if (current_node.airport_id == B) {
-            return current_node;
-        } else if (new Path(current_node).getLength() >= 10) {
-            return null;
-        } else if (current_node == null) {
-            throw new Exception("Cannot find a path. There appears to be either an issue with the algorithm, or the data.");
-        } else {
-            Node bestNode = null;
-            Flight this_flight = current_node.getThisFlight();
-            long time_cost = 0;
-            long layover = 0;
-            for (Flight next_flight : (data.getNextFlights(current_node.getThisFlight()))) {
-                if (this_flight != null) {
-                    var edge_A_side = this_flight.getArrivalDateTime();
-                    var edge_B_side = next_flight.getDepartureDateTime();
-                    layover = ChronoUnit.MINUTES.between(edge_A_side, edge_B_side) - this_flight.getTimezoneOffset();
-                } else if (next_flight.getDestinationAirportId() == B) {
-                    continue;
+    private void recursive(Node current_node, FlightList data, int B) throws Exception {
+        if (new Path(current_node).getLength() >= 10) {
+            if (current_node.airport_id == B) {
+               paths.add(new Path(current_node));
+            } else if (current_node == null) {
+                throw new Exception("Cannot find a path. There appears to be either an issue with the algorithm, or the data.");
+            } else {
+                Flight this_flight = current_node.getThisFlight();
+                long time_cost = 0;
+                long layover = 0;
+                for (Flight next_flight : (data.getNextFlights(current_node.getThisFlight()))) {
+                    if (this_flight != null) {
+                        var edge_A_side = this_flight.getArrivalDateTime();
+                        var edge_B_side = next_flight.getDepartureDateTime();
+                        layover = ChronoUnit.MINUTES.between(edge_A_side, edge_B_side) - this_flight.getTimezoneOffset();
+                    } else if (next_flight.getDestinationAirportId() == B) {
+                        continue;
+                    }
+                    time_cost = layover + next_flight.getFlightTime();
+                    if (new Path(current_node).contains(next_flight.getDestinationAirportId()))
+                        recursive(new Node(
+                                        next_flight.getDestinationAirportId(),
+                                        current_node,
+                                        next_flight,
+                                        data.getNextFlights(next_flight),
+                                        time_cost,
+                                        next_flight.getTicketPrice()),
+                                data, B);
                 }
-                time_cost = layover + next_flight.getFlightTime();
-                if (!visited.contains(next_flight.getDestinationAirportId()) &&
-                        (new Path(recursive(new Node(
-                                        next_flight.getDestinationAirportId(), current_node, next_flight, data.getNextFlights(next_flight), time_cost, next_flight.getTicketPrice()),
-                                visited, data, B)).compareTo(new Path(bestNode)) < 0 || bestNode == null))
-                    bestNode = current_node;
             }
-            return bestNode;
         }
     }
 }
--- src/data/Path.java
+++ src/data/Path.java
@@ -1,22 +1,30 @@
 package data;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.TreeSet;
 
 public class Path implements Comparable<Path> {
     TreeSet<Node> nodes = new TreeSet<>();
+    HashSet<Integer> airports = new HashSet<>();
 
     public Path(Node tail){
         if (tail != null) {
             Node n = tail;
             do {
                 nodes.add(n);
+                airports.add(n.airport_id);
                 n = n.parent;
             } while (n != null);
         }
     }
     public int getLength(){
         return nodes.size();
+    }
+    public boolean contains(int airportID){
+        if (airports.contains(airportID))
+            return true;
+        else return false;
     }
 
 
