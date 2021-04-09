40245e320baa34441ecab03bdfc4c20eb06699f6
Josh Cooper <cooper.s.josh@outlook.com>
2021-04-09 16:49:09 -0700

    Replaces B loop with loop that closer resembles A

--- src/cosc320/AlgorithmB.java
+++ src/cosc320/AlgorithmB.java
@@ -9,48 +9,51 @@ import java.time.temporal.ChronoUnit;
 import java.util.TreeSet;
 
 public class AlgorithmB {
-    private static TreeSet<Path> paths = new TreeSet<>();
-
-    public AlgorithmB() {
-    }
-
     public static Node findPath(int start, int dest, FlightList data) throws Exception {
-        Node start_node = new Node(start, null, null, data.getAllFlights(start), 0, 0);
-        recursive(start_node, data, dest);
-        return paths.first().getTail();
+        try {
+            Node start_node = new Node(start, null, null, data.getAllFlights(start), 0, 0);
+            TreeSet<Path> paths = new TreeSet<>();
+            recursive(paths, start_node, data, dest);
+            if (!paths.isEmpty()) {
+                return paths.first().getTail();
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
-    private static void recursive(Node current_node, FlightList data, int dest) throws Exception {
+    private static void recursive(TreeSet<Path> paths, Node current_node, FlightList data, int dest) throws Exception {
         if (current_node == null)
             throw new Exception("Cannot find a path. There appears to be either an issue with the algorithm, or the data.");
         Path route = new Path(current_node);
-        if (route.getLength() >= 10) {
-            if (current_node.airport_id == dest) {
-                paths.add(route);
-            } else {
+        if (route.getLength() < 10) {
+            if (current_node.airport_id != dest) {
                 Flight this_flight = current_node.getThisFlight();
-                long time_cost = 0;
-                long layover = 0;
-                for (Flight next_flight : data.getNextFlights(current_node.getThisFlight())) {
-                    if (this_flight != null) {
-                        var edge_A_side = this_flight.getArrivalDateTime();
-                        var edge_B_side = next_flight.getDepartureDateTime();
-                        layover = ChronoUnit.MINUTES.between(edge_A_side, edge_B_side) - this_flight.getTimezoneOffset();
-                    } else if (next_flight.getDestinationAirportId() == dest) {
-                        continue;
+                for (int i = 0; i < current_node.getNumNextFlights(); ++i) {
+                    // new outgoing flight
+                    Flight next_flight = current_node.getNextFlight(i);
+                    System.out.println(next_flight);
+                    if (!route.contains(next_flight.getDestinationAirportId())) {
+                        long time_cost = 0;
+                        long layover = 0;
+
+                        if (this_flight != null) {
+                            var edge_A_side = this_flight.getArrivalDateTime();
+                            var edge_B_side = next_flight.getDepartureDateTime();
+                            layover = ChronoUnit.MINUTES.between(edge_A_side, edge_B_side) - this_flight.getTimezoneOffset();
+                        } else if (next_flight.getDestinationAirportId() == dest) {
+                            continue;
+                        }
+                        time_cost = layover + next_flight.getFlightTime();
+                        Node node = new Node(next_flight.getDestinationAirportId(), current_node, next_flight,
+                                data.getNextFlights(next_flight), time_cost, next_flight.getTicketPrice());
+                        recursive(paths, node, data, dest);
                     }
-                    time_cost = layover + next_flight.getFlightTime();
-                    if (route.contains(next_flight.getDestinationAirportId()))
-                        recursive(new Node(
-                                        next_flight.getDestinationAirportId(),
-                                        current_node,
-                                        next_flight,
-                                        data.getNextFlights(next_flight),
-                                        time_cost,
-                                        next_flight.getTicketPrice()),
-                                data, dest);
                 }
             }
+        } else {
+            paths.add(route);
         }
-    }
+        }
 }
